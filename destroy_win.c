42832f11a485a1fd5ac9adc253a7e7c6df44add1
keith.teo2005@gmail.com <keith.teo2005@gmail.com>
2025-04-17 16:37:25 +0800

    made functions to close the window

---
+++ destroy_win.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   destroy_win.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: keteo <keteo@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/09 13:31:56 by keteo             #+#    #+#             */
+/*   Updated: 2025/04/09 13:31:56 by keteo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+int off(int keycode, t_data *mlx)
+{
+	printf("Keycode:%d\n", keycode);
+	if(keycode == 65307)
+	{	
+		mlx_destroy_window(mlx->mlx, mlx->wind);
+		mlx_destroy_display(mlx->mlx);
+		free(mlx->mlx);
+		exit (0);
+	}
+	return (0);
+}
+
+int x_off(t_data *mlx)
+{
+	mlx_destroy_window(mlx->mlx, mlx->wind);
+	mlx_destroy_display(mlx->mlx);
+	free(mlx->mlx);
+	exit (0);
+	return (0);
+}
