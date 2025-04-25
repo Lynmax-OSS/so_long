be714d7f0ac35e202e12726bacb9e986e1814876
keith.teo2005@gmail.com <keith.teo2005@gmail.com>
2025-04-25 12:17:27 +0800

    implemented map reader

---
+++ src/map_extract.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_parser.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: keteo <keteo@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/17 11:03:25 by keteo             #+#    #+#             */
+/*   Updated: 2025/04/17 11:03:25 by keteo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include "../so_long.h"
+#include "../Libft/libft.h"
+// #include "../../ft_printf/ft_printf.h"
+
+int line_count(char *filepath)
+{
+	t_line_info line;
+
+	line.count = 0;
+	line.fd = open(filepath, O_RDONLY);
+	if(line.fd < 0)
+		exit(1);
+	while ((line.line = get_next_line(line.fd)) != NULL)
+	{
+		line.count++;
+		free(line.line);
+	}
+	close (line.fd);
+	return (line.count);
+}
+
+char **read_map(char *filepath)
+{
+	t_line_info line;
+
+	line.i = 0;
+	line.count = line_count(filepath);
+	line.map = malloc(sizeof(char*) * line.count + 1);
+	if(line.map == NULL)
+	{
+		free (line.map);
+		return (NULL);
+	}
+	line.fd = open(filepath, O_RDONLY);
+	if(line.fd < 0)
+		return (NULL);
+	while ((line.line = get_next_line(line.fd))) 
+	{
+		line.map[line.i++] = line.line;
+		free (line.line);
+	}
+	line.map[line.i] = NULL;
+	close(line.fd);
+	return (line.map);
+}
+
+// int main(void)
+// {
+// 	char **map;
+// 	int i;
+
+// 	i = 0;
+// 	map = read_map("../maps/map_1.ber");
+	
+// 	while (map[i] != NULL)
+// 	{
+// 		ft_printf("%s\n", map[i]);
+// 		i++;
+// 	}
+// 	free (map);
+// }
