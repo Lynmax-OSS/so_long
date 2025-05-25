6be97ffbf1e2f6d649afa479c1ba08d52f25b5c7
Keith <keith.teo2005@gmail.com>
2025-05-25 14:20:41 +0800

    starting to add the funtions in the main

--- so_long.c
+++ so_long.c
@@ -12,17 +12,27 @@
 
 #include "so_long.h"
 
-int main(int ac, char **av)//Map file path should be theonly arguement needed
+int main(int ac, char **av)
 {
+	t_game game;
+	char **map;
+
 	if (ac == 2)
 	{
-		checkfile_extention (needs path to file)
-		get map (needs path to file to read)
-		validate map (result form get map comes here)
-		get sprites( path to xpm files)
-		render map( displays sprites based on map)
-		(we need to loop this)handle inputs()
-		(same with this)game loops until level cleared
-		exit game(has to detect that the player has unlocked exit picked up collectibles and exited the map)
+		if(!extension_checker(av[1]))
+			return (0);
+		map = read_map(av[1]);
+		if(!map_validation(map))
+			return (0);
+		game.map = map;
+		init_mlx(&game);
+		ass_loader(&game);
+		mlx_loop_hook(game.mlx, render_map, &game);
+		mlx_loop(game.mlx);
+		// get sprites( path to xpm files)
+		// render map( displays sprites based on map)
+		// (we need to loop this)handle inputs()
+		// (same with this)game loops until level cleared
+		// exit game(has to detect that the player has unlocked exit picked up collectibles and exited the map)
 	}
 }
